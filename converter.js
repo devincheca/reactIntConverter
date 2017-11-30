629279f71d09f90fc248399f8265d8bef60a338e
Checa Devin <devin_checa@elcamino.edu>
2017-11-30 15:19:15 -0800

    began drafting functions for conversion

---
+++ converter.js
@@ -0,0 +1,48 @@
+﻿function fromDecimalToBinary(decimal)
+{
+  
+}
+function fromOctalToBinary(octal)
+{
+  
+}
+function fromHexToBinary(hex)
+{
+  
+}
+function fromBinaryToOctal(binary)
+{
+  
+}
+function fromDecimalToOctal(decimal)
+{
+  
+}
+function fromHexToOctal(hex)
+{
+  
+}
+function fromBinaryToDecimal(binary)
+{
+  
+}
+function fromOctalToDecimal(octal)
+{
+  
+}
+function fromHexToDecimal(hex)
+{
+  
+}
+function fromBinaryToHex(binary)
+{
+  
+}
+function fromDecimalToHex(decimal)
+{
+  
+}
+function fromOctalToHex(octal)
+{
+  
+}
